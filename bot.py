b3ec0d0307bb8f5ffe997b18bc8e2d044a1602b1
archiedobert-cyber <archie.dobert@hotmail.com>
2026-09-21 19:13:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -271,21 +271,21 @@ def to_embed(sbc):
 
     if sbc["requirements"]:
         description += (
-            "\n## 🧩 Requirements\n"
+            "\n### 🧩 Requirements\n"
             + "\n".join(sbc["requirements"])
         )
 
     if sbc["rewards"]:
         description += (
-            "\n## 🎁 Rewards\n"
+            "\n### 🎁 Rewards\n"
             + "\n".join(sbc["rewards"])
         )
 
     if sbc["repeatable"]:
-        description += f"\n## 🔁 Repeatable\n{sbc['repeatable']}"
+        description += f"\n **🔁 Repeatable**\n{sbc['repeatable']}"
 
     if sbc["expires"]:
-        description += f"\n## ⏰ Available for\n{sbc['expires']}"
+        description += f"\n **⏰ Available for**\n{sbc['expires']}"
 
     embed = {
         "description": description.strip()[:4000],
