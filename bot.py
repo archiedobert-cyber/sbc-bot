ec234d3e06906bb6e44dde7396de13a0392ba22b
archiedobert-cyber <archie.dobert@hotmail.com>
2026-09-21 13:14:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ NEW_BADGE = re.compile(r"^\s*new\s*$", re.I)
 GENERIC_OG_IMAGE = "fut-social"  # the site-wide fallback image, not SBC-specific
 
 # Title line shown above the SBC cards - edit the text/emojis however you like
-HEADER = "## 🚨🆕 **NEW SBC ALERT** 🆕🚨\n *Hover Bot brought to you by 𝐒𝐊𝐄𝐋𝐄𝐓𝐎𝐑*""
+HEADER = "## 🚨🆕 **NEW SBC ALERT** 🆕🚨\n- *Hover Bot brought to you by 𝐒𝐊𝐄𝐋𝐄𝐓𝐎𝐑*""
 
 # Message posted at the very bottom, after all the SBC cards.
 # Edit the text/emojis/link however you like, or set it to "" for no footer.
